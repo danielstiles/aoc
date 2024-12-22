0b6ea8aa91932acfcdbe0484b4c03f0df244d049
Daniel Stiles <22626981+danielstiles@users.noreply.github.com>
2024-12-22 10:05:45 -0500

    feat: 2024 day 22 optimization

--- 2024/22/internal/parts/parts.go
+++ 2024/22/internal/parts/parts.go
@@ -1,40 +1,41 @@
 package parts
 
 import (
-	"github.com/danielstiles/aoc/pkg/math"
 	"github.com/danielstiles/aoc/pkg/parse"
 )
 
 func Process1(lines []string) (total int) {
 	for _, line := range lines {
-		seed := parse.FindAllInt(line)[0]
+		seed := int(parse.FindAllInt(line)[0])
 		for i := 0; i < 2000; i++ {
 			seed = getNext(seed)
 		}
-		total += seed
+		total += int(seed)
 	}
 	return
 }
 
+const (
+	changeMask = (1 << 20) - 1
+)
+
 func Process2(lines []string) (total int) {
-	options := make(map[int]int)
-	mod := math.Pow(37, 4)
+	options := make([]int, changeMask+1)
+	currOptions := make([]bool, changeMask+1)
 	for _, line := range lines {
-		seqs := make(map[int]int)
 		seed := parse.FindAllInt(line)[0]
 		changes := 0
 		for i := 0; i < 2000; i++ {
 			next := getNext(seed)
-			diff := (next%10 - seed%10) + 18
-			changes = (changes*37 + diff) % mod
-			if _, ok := seqs[changes]; i >= 3 && !ok {
-				seqs[changes] = next % 10
+			diff := (next%10 - seed%10) + 9
+			changes = (changes<<5 + diff) & changeMask
+			if i >= 3 && currOptions[changes] == false {
+				currOptions[changes] = true
+				options[changes] += next % 10
 			}
 			seed = next
 		}
-		for opt, val := range seqs {
-			options[opt] += val
-		}
+		clear(currOptions)
 	}
 	for _, val := range options {
 		total = max(total, val)
@@ -42,12 +43,13 @@ func Process2(lines []string) (total int) {
 	return
 }
 
+const pruneMask = (1 << 24) - 1
+
 func getNext(seed int) int {
 	seed ^= seed << 6
-	seed %= 16777216
+	seed &= pruneMask
 	seed ^= seed >> 5
-	seed %= 16777216
 	seed ^= seed << 11
-	seed %= 16777216
+	seed &= pruneMask
 	return seed
 }
