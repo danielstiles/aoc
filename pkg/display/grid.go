f10c7e174e1c89a86b6f70163dc2d262061caea4
Daniel Stiles <22626981+danielstiles@users.noreply.github.com>
2024-12-28 12:26:09 -0500

    feat: use legacy computing glyphs for grid display

--- pkg/display/grid.go
+++ pkg/display/grid.go
@@ -3,13 +3,13 @@ package display
 import "github.com/danielstiles/aoc/pkg/grid"
 
 // Prints the grid with the givevn map, returning a slice of the printed lines.
-func PrintGrid(g *grid.Grid, key map[int]rune) (lines []string) {
+func PrintGrid(g *grid.Grid, key map[int]string) (lines []string) {
 	for r := g.Size.Row - 1; r >= 0; r -= 1 {
 		var str string
 		for c := 0; c < g.Size.Col; c += 1 {
 			pos := grid.Vec2{Row: r, Col: c}
 			if val, ok := key[g.Get(pos)]; ok {
-				str += string(val)
+				str += val
 			} else {
 				str += "?"
 			}
@@ -20,27 +20,19 @@ func PrintGrid(g *grid.Grid, key map[int]rune) (lines []string) {
 }
 
 var condensedChars = map[int]rune{
-	0:  ' ',
-	1:  '▘',
-	2:  '▝',
-	3:  '▀',
-	4:  '▖',
-	5:  '▌',
-	6:  '▞',
-	7:  '▛',
-	8:  '▗',
-	9:  '▚',
-	10: '▐',
-	11: '▜',
-	12: '▄',
-	13: '▙',
-	14: '▟',
-	15: '█',
+	0:  ' ', 1:  '🬀', 2:  '🬁', 3:  '🬂', 4:  '🬃', 5:  '🬄', 6:  '🬅', 7:  '🬆',
+	8:  '🬇', 9:  '🬈', 10: '🬉', 11: '🬊', 12: '🬋', 13: '🬌', 14: '🬍', 15: '🬎',
+	16: '🬏', 17: '🬐', 18: '🬑', 19: '🬒', 20: '🬓', 21: '▌', 22: '🬔', 23: '🬕',
+	24: '🬖', 25: '🬗', 26: '🬘', 27: '🬙', 28: '🬚', 29: '🬛', 30: '🬜', 31: '🬝',
+	32: '🬞', 33: '🬟', 34: '🬠', 35: '🬡', 36: '🬢', 37: '🬣', 38: '🬤', 39: '🬥',
+	40: '🬦', 41: '🬧', 42: '▐', 43: '🬨', 44: '🬩', 45: '🬪', 46: '🬫', 47: '🬬',
+	48: '🬭', 49: '🬮', 50: '🬯', 51: '🬰', 52: '🬱', 53: '🬲', 54: '🬳', 55: '🬴',
+	56: '🬵', 57: '🬶', 58: '🬷', 59: '🬸', 60: '🬹', 61: '🬺', 62: '🬻', 63: '█',
 }
 
 // Prints a grid containing only 1s and 0s in a condensed form for display.
 func PrintCondensedGrid(g *grid.Grid) (lines []string) {
-	for r := g.Size.Row - 1; r >= 0; r -= 2 {
+	for r := g.Size.Row - 1; r >= 0; r -= 3 {
 		var str string
 		for c := 0; c < g.Size.Col; c += 2 {
 			pos := grid.Vec2{Row: r, Col: c}
@@ -56,6 +48,11 @@ func PrintCondensedGrid(g *grid.Grid) (lines []string) {
 			cell += 4 * g.Get(pos)
 			if nextColumn {
 				cell += 8 * g.Get(pos.Move(grid.Right, 1))
+			}
+			pos = pos.Move(grid.Down, 1)
+			cell += 16 * g.Get(pos)
+			if nextColumn {
+				cell += 32 * g.Get(pos.Move(grid.Right, 1))
 			}
 			str += string(condensedChars[cell])
 		}
